c7067d8ebdfb89baaa6fd59fed9920a49a539b30
dimitris47 <dimitrisinbox@gmail.com>
2021-02-10 17:05:12 +0200

    add classes

--- src/Frequency.java
+++ src/Frequency.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Frequency {
+    static double[] defFreq = {261.626, 277.183, 293.665, 311.127, 329.626, 349.228,
+            369.994, 391.995, 415.305, 440.000, 466.164, 493.883};
+    ArrayList<Double> currFreq = new ArrayList<>(12);
+
+    public Frequency(int pitch) {
+        for (var item : defFreq) {
+            assert false;
+            currFreq.add(item * pitch / 440);
+        }
+    }
 }
--- src/Tone.java
+++ src/Tone.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Tone {
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+public class Tone implements Runnable {
+    public static double freq;
+    public static int ms;
+    public static int volume;
+
+    @Override
+    public void run() {
+        Thread t = Thread.currentThread();
+        byte[] buf = new byte[1];
+        int sampleRate = 44100;
+        AudioFormat af = new AudioFormat(sampleRate, 8, 1, true, false);
+        SourceDataLine sdl = null;
+        try {
+            sdl = AudioSystem.getSourceDataLine(af);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        try {
+            assert sdl != null;
+            sdl.open(af);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        sdl.start();
+        for (double i = 0; i < ms * sampleRate / 1000.0; i++) {
+            double angle = i / (sampleRate / freq) * 2 * Math.PI;
+            buf[0] = (byte) (Math.sin(angle) * volume);
+            sdl.write(buf, 0, 1);
+            if (t.isInterrupted()) {
+                System.out.println("Interrupted");
+                t.interrupt();
+                return;
+            }
+        }
+        sdl.drain();
+        sdl.stop();
+        sdl.close();
+    }
 }
