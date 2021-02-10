702c2b49bdd1c5a19960b36ccee7ecb0d7802b3a
dimitris47 <dimitrisinbox@gmail.com>
2021-02-10 19:54:16 +0200

    add widgets and actions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
@@ -11,10 +10,14 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class Main extends Application {
-    String[] pitches;
+    String[] notes;
     ArrayList<Button> buttons;
     int selButton;
-    ComboBox<String> combo;
+    ComboBox<String> freqCombo;
+    ComboBox<String> durCombo;
+    Slider vol;
+    Button stop;
+    Button about;
 
     static ArrayList<Double> currFreq;
     static int mSec;
@@ -23,26 +26,47 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        pitches = new String[] {"C", "C\u266F/D\u266D", "D", "D\u266F/E\u266D", "E", "F",
+        notes = new String[] {"C", "C\u266F/D\u266D", "D", "D\u266F/E\u266D", "E", "F",
                 "F\u266F/G\u266D", "G", "G\u266F/A\u266D", "A", "A\u266F/B\u266D", "B"};
 
         buttons = new ArrayList<>(12);
         for (int i = 0; i < 12; i++)
-            buttons.add(i, new Button(pitches[i]));
-        for (int selButton = 0; selButton < buttons.size(); selButton++) {
-            buttons.get(selButton).setText(pitches[selButton]);
-            buttons.get(selButton).setOnAction(e -> btnClick());
+            buttons.add(i, new Button(notes[i]));
+        for (int note = 0; note < buttons.size(); note++) {
+            buttons.get(note).setText(notes[note]);
+            buttons.get(note).setOnAction(e -> btnClick());
         }
 
         currFreq = new ArrayList<>(12);
         ArrayList<String> aFreq = new ArrayList<>(97);
         for (int i = 392; i < 490; i++)
             aFreq.add("a = " + i + "Hz");
-        combo = new ComboBox<>();
-        combo.getItems().addAll(aFreq);
+        freqCombo = new ComboBox<>();
+        freqCombo.getItems().addAll(aFreq);
+        freqCombo.setValue("a = 440Hz");
+        freqCombo.setOnAction(e -> freqClick());
+
+        ArrayList<String> dur = new ArrayList<>();
+        for (int i = 1; i < 11; i++)
+            dur.add(i + " sec");
+        durCombo = new ComboBox<>();
+        durCombo.getItems().addAll(dur);
+        durCombo.setValue("1 sec");
+        freqCombo.setOnAction(e -> durClick());
+
+        vol = new Slider();
+        vol.setValue(50);
+        vol.setTooltip(new Tooltip("Volume"));
+        vol.setOnDragDetected(e -> volChanged());
+
+        stop = new Button("Stop");
+        stop.setOnAction(e -> stopPressed());
 
         HBox freqBox = new HBox();
-        freqBox.getChildren().add(combo);
+        freqBox.getChildren().add(freqCombo);
+        freqBox.getChildren().add(vol);
+        freqBox.getChildren().add(durCombo);
+        freqBox.getChildren().add(stop);
         freqBox.setAlignment(Pos.TOP_CENTER);
 
         TilePane tile = new TilePane();
@@ -54,9 +78,16 @@ public class Main extends Application {
         for (Button button : buttons)
             button.setMinSize(64, 16);
 
+        HBox infoBox = new HBox();
+        about = new Button("About");
+        about.setOnAction(e -> aboutClicked());
+        infoBox.getChildren().add(about);
+        infoBox.setAlignment(Pos.BOTTOM_CENTER);
+
         VBox box = new VBox();
         box.getChildren().add(freqBox);
         box.getChildren().add(tile);
+        box.getChildren().add(infoBox);
         box.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(box, 420, 128);
@@ -73,6 +104,52 @@ public class Main extends Application {
         Tone.ms = mSec;
         Tone.volume = volume;
         sound.start();
+    }
+
+    public void freqClick() {
+        String[] item = freqCombo.getValue().split("a = ");
+        String[] stripped = item[1].split("Hz");
+        int selection = Integer.parseInt(stripped[0]);
+        Frequency f = new Frequency(selection);
+        currFreq = f.currFreq;
+    }
+
+    public void durClick() {
+        String[] item = durCombo.getValue().split(" sec");
+        int selection = Integer.parseInt(item[0]);
+        mSec = selection * 1000;
+    }
+
+    public void volChanged() {
+        volume = (int) vol.getValue();
+    }
+
+    public void stopPressed() {
+        int signal = 0;
+        while (!sound.isInterrupted()) {
+            for (int i = 0; i < mSec * 10; i++)
+                sound.interrupt();
+            System.out.println("sent signal " + signal);
+            if (sound.isInterrupted())
+                return;
+        }
+    }
+
+    public void aboutClicked() {
+        String info = """
+                Program created by Dimitris Psathas
+
+                Written in Java, utilizing the JavaFX toolkit
+
+                Published under the GPLv3 License
+
+                \u00A9 2021 Dimitris Psathas""";
+        Dialog<String> infoDialog = new Dialog<>();
+        infoDialog.setTitle("Program info");
+        ButtonType type = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+        infoDialog.setContentText(info);
+        infoDialog.getDialogPane().getButtonTypes().add(type);
+        infoDialog.showAndWait();
     }
 
     public static void main(String[] args) {
